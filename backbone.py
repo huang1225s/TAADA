f9e3c1b9655b0b4fc2f9994efbc8a6f3986dad7a
Yi Huang <58938975+hu1314520@users.noreply.github.com>
2024-10-17 10:50:57 +0800

    Add files via upload

---
+++ backbone.py
@@ -0,0 +1,371 @@
+import numpy as np
+import torch
+import torch.nn as nn
+import torchvision
+from torchvision import models
+from torch.autograd import Variable
+import math
+from HybridSN import HybridSN
+# convnet without the last layer
+class AlexNetFc(nn.Module):
+    def __init__(self):
+        super(AlexNetFc, self).__init__()
+        model_alexnet = models.alexnet(pretrained=True)
+        self.features = model_alexnet.features
+        self.classifier = nn.Sequential()
+        for i in range(6):
+            self.classifier.add_module("classifier"+str(i), model_alexnet.classifier[i])
+
+    def forward(self, x):
+        x = self.features(x)
+        x = x.view(x.size(0), 256*6*6)
+        x = self.classifier(x)
+        return x
+
+
+class ResNet18Fc(nn.Module):
+    def __init__(self, in_channels):
+        super(ResNet18Fc, self).__init__()
+        model_resnet18 = models.resnet18(pretrained=False)
+        # self.conv1 = model_resnet18.conv1
+        self.conv1 = nn.Conv2d(in_channels, 64, kernel_size=7, stride=2, padding=3, bias=False)
+        self.bn1 = model_resnet18.bn1
+        self.relu = model_resnet18.relu
+        self.maxpool = model_resnet18.maxpool
+        self.layer1 = model_resnet18.layer1
+        self.layer2 = model_resnet18.layer2
+        self.layer3 = model_resnet18.layer3
+        self.layer4 = model_resnet18.layer4
+        self.avgpool = model_resnet18.avgpool
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+        x = self.avgpool(x)
+        x = x.view(x.size(0), -1)
+        return x
+
+class ResNet34Fc(nn.Module):
+    def __init__(self):
+        super(ResNet34Fc, self).__init__()
+        model_resnet34 = models.resnet34(pretrained=True)
+        self.conv1 = model_resnet34.conv1
+        self.bn1 = model_resnet34.bn1
+        self.relu = model_resnet34.relu
+        self.maxpool = model_resnet34.maxpool
+        self.layer1 = model_resnet34.layer1
+        self.layer2 = model_resnet34.layer2
+        self.layer3 = model_resnet34.layer3
+        self.layer4 = model_resnet34.layer4
+        self.avgpool = model_resnet34.avgpool
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+        x = self.avgpool(x)
+        x = x.view(x.size(0), -1)
+        return x
+
+class ResNet50Fc(nn.Module):
+    def __init__(self, in_channels):
+        super(ResNet50Fc, self).__init__()
+        model_resnet50 = models.resnet50(pretrained=True)
+        # self.conv1 = model_resnet50.conv1
+        self.conv1 = nn.Conv2d(in_channels, 64, kernel_size=7, stride=2, padding=3, bias=False)
+        self.bn1 = model_resnet50.bn1
+        self.relu = model_resnet50.relu
+        self.maxpool = model_resnet50.maxpool
+        self.layer1 = model_resnet50.layer1
+        self.layer2 = model_resnet50.layer2
+        self.layer3 = model_resnet50.layer3
+        self.layer4 = model_resnet50.layer4
+        self.avgpool = model_resnet50.avgpool
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+        x = self.avgpool(x)
+        x = x.view(x.size(0), -1)
+        return x
+
+class ResNet101Fc(nn.Module):
+    def __init__(self):
+        super(ResNet101Fc, self).__init__()
+        model_resnet101 = models.resnet101(pretrained=True)
+        self.conv1 = model_resnet101.conv1
+        self.bn1 = model_resnet101.bn1
+        self.relu = model_resnet101.relu
+        self.maxpool = model_resnet101.maxpool
+        self.layer1 = model_resnet101.layer1
+        self.layer2 = model_resnet101.layer2
+        self.layer3 = model_resnet101.layer3
+        self.layer4 = model_resnet101.layer4
+        self.avgpool = model_resnet101.avgpool
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+        x = self.avgpool(x)
+        x = x.view(x.size(0), -1)
+        return x
+
+class ResNet152Fc(nn.Module):
+    def __init__(self):
+        super(ResNet152Fc, self).__init__()
+        model_resnet152 = models.resnet152(pretrained=True)
+        self.conv1 = model_resnet152.conv1
+        self.bn1 = model_resnet152.bn1
+        self.relu = model_resnet152.relu
+        self.maxpool = model_resnet152.maxpool
+        self.layer1 = model_resnet152.layer1
+        self.layer2 = model_resnet152.layer2
+        self.layer3 = model_resnet152.layer3
+        self.layer4 = model_resnet152.layer4
+        self.avgpool = model_resnet152.avgpool
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+        x = self.avgpool(x)
+        x = x.view(x.size(0), -1)
+        return x
+
+class ResNet(nn.Module):
+
+    def __init__(self, block, layers, in_channels, num_classes=1000):
+        self.inplanes = 64
+        super(ResNet, self).__init__()
+        self.conv1 = nn.Conv2d(in_channels, 64, kernel_size=7, stride=2, padding=3,
+                               bias=False)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.relu = nn.ReLU(inplace=True)
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        self.layer1 = self._make_layer(block, 64, layers[0])
+        self.layer2 = self._make_layer(block, 128, layers[1], stride=1)
+        self.layer3 = self._make_layer(block, 256, layers[2], stride=1)
+        self.layer4 = self._make_layer(block, 512, layers[3], stride=1)
+        self.avgpool = nn.AvgPool2d(3, stride=1)
+        self.baselayer = [self.conv1, self.bn1, self.layer1, self.layer2, self.layer3, self.layer4]
+        self.fc = nn.Linear(512 * block.expansion, num_classes)
+
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+                m.weight.data.normal_(0, math.sqrt(2. / n))
+            elif isinstance(m, nn.BatchNorm2d):
+                m.weight.data.fill_(1)
+                m.bias.data.zero_()
+
+    def _make_layer(self, block, planes, blocks, stride=1):
+        downsample = None
+        if stride != 1 or self.inplanes != planes * block.expansion:
+            downsample = nn.Sequential(
+                nn.Conv2d(self.inplanes, planes * block.expansion,
+                          kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(planes * block.expansion),
+            )
+
+        layers = []
+        layers.append(block(self.inplanes, planes, stride, downsample))
+        self.inplanes = planes * block.expansion
+        for i in range(1, blocks):
+            layers.append(block(self.inplanes, planes))
+
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
+
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+        x = self.avgpool(x)
+        x = x.view(x.size(0), -1)
+
+        return x
+
+
+class ResBase(nn.Module):
+    def __init__(self, N_BANDS, num_class, option, pret=True):
+        super(ResBase, self).__init__()
+        self.dim = 2048
+        if option == 'resnet18':
+            model_ft = ResNet18Fc(N_BANDS)
+            self.dim = 512
+        if option == 'resnet50':
+            model_ft = models.resnet50(pretrained=pret)
+        if option == 'resnet101':
+            model_ft = models.resnet101(pretrained=pret)
+        if option == 'resnet152':
+            model_ft = models.resnet152(pretrained=pret)
+        if option == 'HybridSN':
+            model_ft = HybridSN()
+        if option == 'DCRN':
+            model_ft = DCRN(N_BANDS,7,num_class)
+        mod = list(model_ft.children())
+        mod.pop()
+        self.model_ft =model_ft
+        self.features = nn.Sequential(*mod)
+
+    def forward(self, x):
+
+        x = self.features(x)
+
+        x = x.view(x.size(0), -1)
+        return x
+
+class DCRN(nn.Module):
+
+    def __init__(self, input_channels, patch_size, n_classes):
+        super(DCRN, self).__init__()
+        self.kernel_dim = 1
+        self.feature_dim = input_channels
+        self.sz = patch_size
+        # Convolution Layer 1 kernel_size = (1, 1, 7), stride = (1, 1, 2), output channels = 24
+        self.conv1 = nn.Conv3d(1, 24, kernel_size=(7, 1, 1), stride=(2, 1, 1), bias=True)
+        self.bn1 = nn.BatchNorm3d(24)
+        self.activation1 = nn.ReLU()
+
+        # Residual block 1
+        self.conv2 = nn.Conv3d(24, 24, kernel_size=(7, 1, 1), stride=1, padding=(3, 0, 0), padding_mode='replicate',
+                               bias=True)
+        self.bn2 = nn.BatchNorm3d(24)
+        self.activation2 = nn.ReLU()
+        self.conv3 = nn.Conv3d(24, 24, kernel_size=(7, 1, 1), stride=1, padding=(3, 0, 0), padding_mode='replicate',
+                               bias=True)
+        self.bn3 = nn.BatchNorm3d(24)
+        self.activation3 = nn.ReLU()
+        # Finish
+
+        # Convolution Layer 2 kernel_size = (1, 1, (self.feature_dim - 6) // 2), output channels = 128
+        self.conv4 = nn.Conv3d(24, 128, kernel_size=(((self.feature_dim - 7) // 2 + 1), 1, 1), bias=True)
+        self.bn4 = nn.BatchNorm3d(128)
+        self.activation4 = nn.ReLU()
+
+        # Convolution layer for spatial information
+        self.conv5 = nn.Conv3d(1, 24, (self.feature_dim, 1, 1))
+        self.bn5 = nn.BatchNorm3d(24)
+        self.activation5 = nn.ReLU()
+
+        # Residual block 2
+        self.conv6 = nn.Conv3d(24, 24, kernel_size=(1, 3, 3), stride=1, padding=(0, 1, 1), padding_mode='replicate',
+                               bias=True)
+        self.bn6 = nn.BatchNorm3d(24)
+        self.activation6 = nn.ReLU()
+        self.conv7 = nn.Conv3d(24, 24, kernel_size=(1, 3, 3), stride=1, padding=(0, 1, 1), padding_mode='replicate',
+                               bias=True)
+        self.bn7 = nn.BatchNorm3d(24)
+        self.activation7 = nn.ReLU()
+        self.conv8 = nn.Conv3d(24, 24, kernel_size=1)
+        # Finish
+
+        # Combination shape
+        self.inter_size = 128 + 24
+
+        # Residual block 3
+        self.conv9 = nn.Conv3d(self.inter_size, self.inter_size, kernel_size=(1, 3, 3), stride=1, padding=(0, 1, 1),
+                               padding_mode='replicate', bias=True)
+        self.bn9 = nn.BatchNorm3d(self.inter_size)
+        self.activation9 = nn.ReLU()
+        self.conv10 = nn.Conv3d(self.inter_size, self.inter_size, kernel_size=(1, 3, 3), stride=1, padding=(0, 1, 1),
+                                padding_mode='replicate', bias=True)
+        self.bn10 = nn.BatchNorm3d(self.inter_size)
+        self.activation10 = nn.ReLU()
+
+        # Average pooling kernel_size = (5, 5, 1)
+        self.avgpool = nn.AvgPool3d((1, self.sz, self.sz))
+
+        # Fully connected Layer
+        self.fc1 = nn.Linear(in_features=self.inter_size, out_features=n_classes)
+
+        # parameters initialization
+        for m in self.modules():
+            if isinstance(m, nn.Conv3d):
+                torch.nn.init.kaiming_normal_(m.weight.data)
+                m.bias.data.zero_()
+            elif isinstance(m, nn.BatchNorm3d):
+                m.weight.data.fill_(1)
+                m.bias.data.zero_()
+
+    def forward(self, x, bounds=None):
+        # Convolution layer 1
+        x1 = self.conv1(x)
+        x1 = self.activation1(self.bn1(x1))
+        # Residual layer 1
+        residual = x1
+        x1 = self.conv2(x1)
+        x1 = self.activation2(self.bn2(x1))
+        x1 = self.conv3(x1)
+        x1 = residual + x1
+        x1 = self.activation3(self.bn3(x1))
+
+        # Convolution layer to combine rest
+        x1 = self.conv4(x1)
+        x1 = self.activation4(self.bn4(x1))
+        x1 = x1.reshape(x1.size(0), x1.size(1), x1.size(3), x1.size(4))
+
+        x2 = self.conv5(x)
+        x2 = self.activation5(self.bn5(x2))
+
+        # Residual layer 2
+        residual = x2
+        residual = self.conv8(residual)
+        x2 = self.conv6(x2)
+        x2 = self.activation6(self.bn6(x2))
+        x2 = self.conv7(x2)
+        x2 = residual + x2
+
+        x2 = self.activation7(self.bn7(x2))
+        x2 = x2.reshape(x2.size(0), x2.size(1), x2.size(3), x2.size(4))
+
+        # concat spatial and spectral information
+        x = torch.cat((x1, x2), 1)
+
+        x = self.avgpool(x)
+        x = x.reshape((x.size(0), -1))
+
+        # Fully connected layer
+        # x = self.fc1(x)
+
+        return x
+
+network_dict = {"AlexNet": AlexNetFc,
+                "ResNet18": ResNet18Fc,
+                "ResNet34": ResNet34Fc,
+                "ResNet50": ResNet50Fc,
+                "ResNet101": ResNet101Fc,
+                "ResNet152": ResNet152Fc}

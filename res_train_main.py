4f1b85e4b260e9f7e960352ec599faa38feb31e7
Yi Huang <58938975+huang1225s@users.noreply.github.com>
2025-06-11 21:35:12 +0800

    Update res_train_main.py

--- res_train_main.py
+++ res_train_main.py
@@ -17,7 +17,7 @@ parser.add_argument('--batch-size', type=int, default=128, metavar='N',
                     help='input batch size for training (default: 64)')
 parser.add_argument('--num_epoch', type=int, default=100, metavar='N',
                     help='number of epochs to train (default: 10)')
-parser.add_argument('--lr', type=float, default=0.01, metavar='LR',
+parser.add_argument('--lr', type=float, default=0.1, metavar='LR',
                     help='learning rate (default: 0.1)')
 parser.add_argument('--momentum', type=float, default=0.9, metavar='M',
                     help='SGD momentum (default: 0.5)')

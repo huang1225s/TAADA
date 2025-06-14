0740cdfde5ae4d208b5f5c9a8fa86ca0a9ae7220
Yi Huang <58938975+huang1225s@users.noreply.github.com>
2025-06-14 20:33:42 +0800

    Update res_train_main.py

--- res_train_main.py
+++ res_train_main.py
@@ -17,7 +17,7 @@ parser.add_argument('--batch-size', type=int, default=128, metavar='N',
                     help='input batch size for training (default: 64)')
 parser.add_argument('--num_epoch', type=int, default=100, metavar='N',
                     help='number of epochs to train (default: 10)')
-parser.add_argument('--lr', type=float, default=0.1, metavar='LR',
+parser.add_argument('--lr', type=float, default=0.01, metavar='LR',
                     help='learning rate (default: 0.1)')
 parser.add_argument('--momentum', type=float, default=0.9, metavar='M',
                     help='SGD momentum (default: 0.5)')
@@ -29,7 +29,7 @@ parser.add_argument('--seed', type=int, default=1, metavar='S',
                     help='random seed (default: 1)')
 parser.add_argument('--log-interval', type=int, default=5, metavar='N',
                     help='how many batches to wait before logging training status')
-parser.add_argument('--num_k', type=int, default=3, metavar='K',
+parser.add_argument('--num_k', type=int, default=4, metavar='K',
                     help='how many steps to repeat the generator update')
 parser.add_argument('--name', type=str, default='board', metavar='B',
                     help='board dir')
